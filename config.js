895e1631f6400942b9f825ccb6b53d759447852a
DowanKim <kimdowan1004@naver.com>
2025-05-04 17:26:24 +0900

    Remove config.js from tracking

--- config.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "35a8914aa47586edc9e38aad04aaaf2e";  // 여기는 git에 절대 올리지 말기
-export const BASE_URL = "https://api.themoviedb.org/3";
-export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
